870bfceb3ee32699e19cbd4596ef6d5060bf093f
pro-soichiro <dr.1987626@gmail.com>
2022-11-12 13:58:21 +0900

    add images and setting constant

---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+// REQUEST_STATEはAPIリクエスト中に画面の状態を知るための定数
+export const REQUEST_STATE = {
+  INITIAL: 'INITIAL',
+  LOADING: 'LOADING',
+  OK: 'OK',
+}
+
+export const HTTP_STATUS_CODE = {
+  NOT_ACCEPTABLE: 406,
+}
