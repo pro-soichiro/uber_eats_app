758d09f67a52d9b58598e7f7fce40a58f91914b7
pro-soichiro <dr.1987626@gmail.com>
2022-11-12 15:41:18 +0900

    add axios and rack-cors, make endpoint

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+import { fetchRestaurants } from "../apis/restaurants";
+
 export const Restaurants = () => {
+
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>  console.log(data))
+    }, [])
   return (
     <>
       <h1>レストラン一覧</h1>
