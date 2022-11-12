b94f816ddc726f46d58b86e999e08139c7c2d48c
pro-soichiro <dr.1987626@gmail.com>
2022-11-12 17:25:21 +0900

    done restaurants.jsx

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,15 +1,119 @@
-import { useEffect } from "react"
+import { useEffect, useReducer } from "react"
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import Skeleton from '@mui/material/Skeleton';
+
+// api
 import { fetchRestaurants } from "../apis/restaurants";
+
+// reducers
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer,
+} from '../reducers/restaurants';
+
+// constants
+import { REQUEST_STATE } from "../constants"
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantsImage  from '../images/restaurant-image.jpg';
+
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`
+
+const MainCover = styled.img`
+  height: 600px;
+`
+
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`
 
 export const Restaurants = () => {
 
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
   useEffect(() => {
+    dispatch({type: restaurantsActionTypes.FETCHING });
+
     fetchRestaurants()
-    .then((data) =>  console.log(data))
-    }, [])
+    .then((data) =>
+      dispatch({
+        type: restaurantsActionTypes.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants
+        }
+      })
+    )
+  }, [])
+
+
+
   return (
     <>
-      <h1>レストラン一覧</h1>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+      <RestaurantsContentsList>
+      {
+        state.fetchState === REQUEST_STATE.LOADING?
+          <>
+            <Skeleton variant="rect" width={450} height={300} />
+            <Skeleton variant="rect" width={450} height={300} />
+            <Skeleton variant="rect" width={450} height={300} />
+          </>
+        :
+          state.restaurantsList.map((item, index) =>
+            <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none'}}>
+              <RestaurantsContentWrapper>
+                <RestaurantsImageNode src={RestaurantsImage} />
+                <MainText>{item.name}</MainText>
+                <SubText>{`配送料:${item.fee}円 ${item.time_required}分`}</SubText>
+              </RestaurantsContentWrapper>
+            </Link>
+          )
+      }
+      </RestaurantsContentsList>
     </>
   )
 }
