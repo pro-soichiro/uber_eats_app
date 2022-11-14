f59e6e452511a4602428ba07eba802266583f7bf
pro-soichiro <dr.1987626@gmail.com>
2022-11-14 21:08:11 +0900

    注文ページに表示する仮注文詳細のAPIを叩いて、コンソールに表示する

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useReducer } from "react";
+
+// api
+import { fetchLineFoods } from "../apis/line_foods";
+
+// reducer
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
+
 export const Orders = () => {
-  return (
-    <>
-      <h1>注文画面</h1>
-    </>
-  )
-}
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    fetchLineFoods()
+    .then((data) => {
+      dispatch({
+        type: lineFoodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          lineFoodsSummary: data,
+        },
+      });
+    })
+    .catch((e) => console.error(e));
+  }, []);
+
+  return <>注文画面{state.lineFoodsSummary && state.lineFoodsSummary.count}</>;
+};
